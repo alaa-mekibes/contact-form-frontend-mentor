2c3e1f155db460cd1470f78839362de20dbd352e
Alaa Mekibes <alaamks115@gmail.com>
2025-05-26 15:25:50 +0100

    fix 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,12 +14,16 @@ class FormVerification {
 
     displayError(place, text) {
         const span = document.createElement("span");
-        span.id = "message-error";
+        switch (place) {
+            case this.queryTypeContainer : span.id = `qt-error`; break;
+            case this.termsContainer : span.id = `at-error`; break;
+            default : span.id = `${place.name}-error`;
+        }
         span.setAttribute("role", "alert");
         span.classList.add("error");
         span.textContent = `${text}`;
         place.style.borderColor = "hsl(0, 66%, 54%)";
-        if(place.nextElementSibling?.id === "message-error") {
+        if(place.nextElementSibling?.classList.contains("error")) {
             span.remove();
         }
         else {
@@ -29,6 +33,7 @@ class FormVerification {
 
     init(place, field) {
         field.setAttribute("aria-invalid", "false");
+        field.removeAttribute("aria-decribedby");
         place.style.borderColor = "hsl(187, 24%, 22%)";
         const parent = place.parentElement;
         const allChilds = Array.from(parent.children);
@@ -44,31 +49,37 @@ class FormVerification {
                 if(!this.firstName[0].value) {
                     this.displayError(this.firstName[0], "This field is required");
                     this.firstName[0].setAttribute("aria-invalid", "true");
+                    this.firstName[0].setAttribute("aria-decribedby", "fn-error");
                 }
                 else this.init(this.firstName[0], this.firstName[0]);
                 if(!this.lastName[0].value) {
                     this.displayError(this.lastName[0], "This field is required");
                     this.lastName[0].setAttribute("aria-invalid", "true");
+                    this.lastName[0].setAttribute("aria-decribedby", "ln-error");
                 }
                 else this.init(this.lastName[0], this.lastName[0]);
                 if(!this.email[0].value) {
                     this.displayError(this.email[0], "Please enter a valid email address");
                     this.email[0].setAttribute("aria-invalid", "true");
+                    this.email[0].setAttribute("aria-decribedby", "ea-error");
                 }
                 else this.init(this.email[0], this.email[0]);
                 if(!this.queryType[0].checked && !this.queryType[1].checked) {
                     this.displayError(this.queryTypeContainer, "Please select a query type");
                     this.queryType[0].setAttribute("aria-invalid", "true");
+                    this.queryType[0].setAttribute("aria-decribedby", "qt-error");
                 }
                 else this.init(this.queryTypeContainer, this.queryType[0]);
                 if(!this.message[0].value) {
                     this.displayError(this.message[0], "This field is required");
                     this.message[0].setAttribute("aria-invalid", "true");
+                    this.message[0].setAttribute("aria-decribedby", "msg-error");
                 }
                 else this.init(this.message[0], this.message[0]);
                 if(!this.terms[0].checked) {
                     this.displayError(this.termsContainer, "To submit this form, please consent to being contacted");
                     this.terms[0].setAttribute("aria-invalid", "true");
+                    this.terms[0].setAttribute("aria-decribedby", "at-error");
                 }
                 else this.init(this.termsContainer, this.terms[0])
 
